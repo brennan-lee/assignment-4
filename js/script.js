d078da11f848cad38e40ade66c30cd991c5aeff6
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-12-04 14:07:04 -0500

    update

--- js/script.js
+++ js/script.js
@@ -10,14 +10,49 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/assignment-4/sw.js", {
+    scope: "/assignment-4/",
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  let userSize = document.getElementById("size").value
+  let userdrink = document.getElementById("kind-of-drink").value
+
+  if (userSize == "Small") {
+    let sizePrice = 1.0
+    document.getElementById("drink").innerHTML =
+      "You want to order a small soft drink."
+  } else if (userSize == "Medium") {
+    let sizePrice = 1.5
+    document.getElementById("drink").innerHTML =
+      "You want to order a medium soft drink."
+  } else if (userSize == "Large") {
+    let sizePrice = 2.0
+    document.getElementById("drink").innerHTML =
+      "You want to order a large soft drink."
+  } else {
+    document.getElementById("drink").innerHTML = "Error"
+  }
+
+  if (userdrink == "cola") {
+    let topingPrice = 0.5
+    document.getElementById("kind-of-drink").innerHTML =
+      "You want to add cola."
+  } else if (userdrink == "Sprite") {
+    let topingPrice = 1.0
+    document.getElementById("kind-of-drink").innerHTML =
+      "You want to add Sprite."
+  } else if (userdrink == "pepsi") {
+    let topingPrice = 1.5
+    document.getElementById("kind-of-drink").innerHTML =
+      "You want pepsi."
+  } else {
+    document.getElementById("kind-of-drink").innerHTML = "Error"
+  }
+
+  let totalPrice = sizePrice + drinkPrice
+
+  document.getElementById("total").innerHTML =
+    "Your total will be $" + totalPrice 
 }
